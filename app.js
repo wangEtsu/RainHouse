db68f25cf8b352fe02c7ec036da72b8cf8eae53a
wangetsu1229@gmail.com <wangetsu1229@gmail.com>
2020-04-08 17:48:42 +1000

    Update server setting, add Procfile, read for test deployment

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
-const app = express;
+const app = express();
 
-app.request(express.static("public"));
+app.use(express.static("public"));
 
-app.request(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
     extended: true
 }))
 
@@ -15,3 +15,8 @@ app.get("/", function(req, res) {
     console.log("Server is running on port 000");
     res.sendFile(__dirname + "/index.html")
 })
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Server is running on port 3000");
+});
+
