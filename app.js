61af84f5625be475ac2d1e158d3c4e4fadb67696
wangetsu1229@gmail.com <wangetsu1229@gmail.com>
2020-04-10 12:53:25 +1000

    Refact with ejs

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
+const homeStartingContent = "Insert description for the home page";
+
+// Initialize express
 const app = express();
 
+// Set view engine to ejs
+app.set("view engine", "ejs");
+
+// Set up public folder to store static file including css and images
 app.use(express.static("public"));
 
+// Set up bodyparser encode method
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+// Re
 app.get("/", function(req, res) {
-    console.log("Server is running on port 000");
-    res.sendFile(__dirname + "/index.html")
+    res.render("home", {
+        startingContent: homeStartingContent
+    })
 })
 
 app.listen(process.env.PORT || 3000, function() {
