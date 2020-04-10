299d3abd14a504363787c409ae3e1e78b7844d8a
wangetsu1229@gmail.com <wangetsu1229@gmail.com>
2020-04-10 16:08:24 +1000

    Update About and Contact pages, update nav bar

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 
-const homeStartingContent = "Insert description for the home page";
+const homeStartingContent = "Insert description of the home page";
+const contactContent = "Insert description of the contact page";
+const aboutContent = "Insert description of the about page"
 
 // Initialize express
 const app = express();
@@ -25,6 +27,25 @@ app.get("/", function(req, res) {
         startingContent: homeStartingContent
     })
 })
+
+app.get("/contact", function(req, res) {
+    res.render("home", {
+        startingContent: contactContent
+    })
+})
+
+app.get("/contact", function(req, res) {
+    res.render("contact", {
+        startingContent: contactContent
+    })
+})
+
+app.get("/about", function(req, res) {
+    res.render("about", {
+        startingContent: aboutContent
+    })
+})
+
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 3000");
