d63d8a2992a05b466c1e0d6f7da23f34c42c37da
wangetsu1229@gmail.com <wangetsu1229@gmail.com>
2020-04-13 21:37:19 +1000

    Update calculator demo

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,64 +1,152 @@
-// Create an array to store all questions.
-var allQuestions = [];
-// Store each question in an object.
-allQuestions[0] = {
-    question: "How many people lives in your household?",
-    choices: ["3", "4", "5"],
-    correctAnswer: 2
-};
-allQuestions[1] = {
-    question: "How long shower?",
-    choices: ["10-20min", "20-30min", "30-40min"],
-    correctAnswer: 1
-};
-allQuestions[2] = {
-    question: "Large garden",
-    choices: ["small", "medium", "large"],
-    correctAnswer: 0
-};
+(function(){
+  // Functions
+  function buildQuiz(){
+    // variable to store the HTML output
+    const output = [];
 
-// Display first question
-document.getElementById("question").textContent = allQuestions[0].question;
+    // for each question...
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
 
-document.getElementById("choice0").textContent = allQuestions[0].choices[0];
+        // variable to store the list of possible answers
+        const answers = [];
 
-document.getElementById("choice1").textContent = allQuestions[0].choices[1];
+        // and for each available answer...
+        for(letter in currentQuestion.answers){
 
-document.getElementById("choice2").textContent = allQuestions[0].choices[2];
+          // ...add an HTML radio button
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
 
-// Create a variable to store the user's score
-var userScore = 0;
+        // add this question and its answers to the output
+        output.push(
+          `<div class="slide">
+            <div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join("")} </div>
+          </div>`
+        );
+      }
+    );
 
-// Create a variable to store the index of the current question
-var questionNum = 0;
-
-// When the NEXT button is clicked, the user's score is updated, the current question is hidden, and the next question is revealed.
-  $("#next").click(function() {
-   
-  // Check if User answered question.
-  // If so, update user's score. If not, do not continue quiz until answer is given.
-  if($("form input[name=answer]:checked").val() == allQuestions[questionNum].correctAnswer) {
-    userScore++;
+    // finally combine our output list into one string of HTML and put it on the page
+    quizContainer.innerHTML = output.join('');
   }
-  
-  // If last question, show user's score rather than next question
-    if (questionNum == (allQuestions.length - 1)) {
-    document.getElementsByTagName("form")[0].style.display = "none";
-      
-    document.getElementById("question").textContent ="Your score is " + userScore + " out of 3.";
+
+  function showResults(){
+    
+    const usage_dict = {
+      "Dual flush": 50,
+      "Normal flush": 100, 
+      "under 10 minutes": 10,
+      "10 ~ 25 minutes": 25,
+      "over 25 minutes": 50,
+      "Yes": 100,
+      "No": 20
+    };
+
+    // set up list to collect user input
+    let result = [];
+    
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+
+    // for each question...
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      console.log(currentQuestion.answers[userAnswer]);
+      console.log(usage_dict[currentQuestion.answers[userAnswer]]);
+      result.push(currentQuestion.answers[userAnswer]);
+    });
+
+    // show number of correct answers out of total
+    console.log(result);
+    console.log("end");
+    // resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+
+  function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
     }
-    else {
-  
-  // Current question is not the last question, so increment the current question index
-    questionNum++;
-  
-  // Replace current question with next question
-    console.log(questionNum);
-    document.getElementById("question").textContent = allQuestions[questionNum].question;
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+  }
 
-    document.getElementById("choice0").textContent = allQuestions[questionNum].choices[0];
+  function showNextSlide() {
+    showSlide(currentSlide + 1);
+  }
 
-    document.getElementById("choice1").textContent = allQuestions[questionNum].choices[1];
+  function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+  }
 
-    document.getElementById("choice2").textContent = allQuestions[questionNum].choices[2];
-    }});
+  // Variables
+  const quizContainer = document.getElementById('quiz');
+  const resultsContainer = document.getElementById('results');
+  const submitButton = document.getElementById('submit');
+  const myQuestions = [
+    {
+      question: "Do you have a dual flush or normal flush toilet?",
+      answers: {
+        a: "Dual flush",
+        b: "Normal flush",
+      },
+    },
+    {
+      question: "How long does your shower took",
+      answers: {
+        a: "under 10 minutes",
+        b: "10 ~ 25 minutes",
+        c: "over 25 minutes"
+      },
+    },
+    {
+      question: "Do you have a dishwasher",
+      answers: {
+        a: "Yes",
+        b: "No"
+      },
+    }
+  ];
+
+
+  // Kick things off
+  buildQuiz();
+
+  // Pagination
+  const previousButton = document.getElementById("previous");
+  const nextButton = document.getElementById("next");
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  // Show the first slide
+  showSlide(currentSlide);
+
+  // Event listeners
+  submitButton.addEventListener('click', showResults);
+  previousButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+})();
