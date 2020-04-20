3a8a99fdefbef97508a840c78b902d36f35b47a3
Yue Wang <wangetsu1229@gmail.com>
2020-04-21 00:21:01 +1000

    Clean up layout and style, remove section, add amchart bar chart

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -92,7 +92,7 @@ const myQuestions = [
   // Toilet set
   {
     questionTag: "toiletAmount",
-    question: "Do you have a choice between a partial flush or a full flush in your toilet?",
+    question: "Do you have a partial flush or a full flush toilet?",
     activity: "Toilet",
     answers: {
       "540": "Yes",
@@ -235,7 +235,7 @@ function buildQuiz() {
         `<div class="quiz-slide">
             <div class="question"> ${currentQuestion.question} </div>
             <div class="answers"><div class="radio-toolbar"> ${answers.join("")} </div></div>
-            <div class="tip"><i class="fas fa-lightbulb"></i> ${currentQuestion.tip} </div>
+            <div><i class="fas fa-lightbulb"></i> ${currentQuestion.tip} </div>
           </div>`
       );
     }
@@ -521,6 +521,119 @@ function showResults() {
   $("#pie-analysis").html(piePreSet);
 
 
+
+  // Amchart bar
+  am4core.ready(function() {
+
+    // Themes begin
+    am4core.useTheme(am4themes_dataviz);
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+    
+    
+    
+    
+    var chart = am4core.create('chartdiv', am4charts.XYChart)
+    chart.colors.step = 2;
+    
+    chart.legend = new am4charts.Legend()
+    chart.legend.position = 'top'
+    chart.legend.paddingBottom = 20
+    chart.legend.labels.template.maxWidth = 95
+    
+    var xAxis = chart.xAxes.push(new am4charts.CategoryAxis())
+    xAxis.dataFields.category = 'category'
+    xAxis.renderer.cellStartLocation = 0.1
+    xAxis.renderer.cellEndLocation = 0.9
+    xAxis.renderer.grid.template.location = 0;
+    
+    var yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    yAxis.min = 0;
+    
+    function createSeries(value, name) {
+        var series = chart.series.push(new am4charts.ColumnSeries())
+        series.dataFields.valueY = value
+        series.dataFields.categoryX = 'category'
+        series.name = name
+    
+        series.events.on("hidden", arrangeColumns);
+        series.events.on("shown", arrangeColumns);
+    
+        var bullet = series.bullets.push(new am4charts.LabelBullet())
+        bullet.interactionsEnabled = false
+        bullet.dy = 30;
+        bullet.label.text = '{valueY}'
+        bullet.label.fill = am4core.color('#ffffff')
+    
+        return series;
+    }
+    
+    chart.data = [
+        {
+            category: 'Shower & Bath',
+            You: 40,
+            Average: 55,
+        },
+        {
+            category: 'Toilet',
+            You: 30,
+            Average: 78,
+        },
+        {
+            category: 'Kitchen',
+            You: 27,
+            Average: 40,
+        },
+        {
+            category: 'Laundry',
+            You: 50,
+            Average: 33,
+        }
+    ]
+    
+    
+    createSeries('You', 'Your consumption');
+    createSeries('Average', 'Average consumption');
+    
+    function arrangeColumns() {
+    
+        var series = chart.series.getIndex(0);
+    
+        var w = 1 - xAxis.renderer.cellStartLocation - (1 - xAxis.renderer.cellEndLocation);
+        if (series.dataItems.length > 1) {
+            var x0 = xAxis.getX(series.dataItems.getIndex(0), "categoryX");
+            var x1 = xAxis.getX(series.dataItems.getIndex(1), "categoryX");
+            var delta = ((x1 - x0) / chart.series.length) * w;
+            if (am4core.isNumber(delta)) {
+                var middle = chart.series.length / 2;
+    
+                var newIndex = 0;
+                chart.series.each(function(series) {
+                    if (!series.isHidden && !series.isHiding) {
+                        series.dummyData = newIndex;
+                        newIndex++;
+                    }
+                    else {
+                        series.dummyData = chart.series.indexOf(series);
+                    }
+                })
+                var visibleCount = newIndex;
+                var newMiddle = visibleCount / 2;
+    
+                chart.series.each(function(series) {
+                    var trueIndex = chart.series.indexOf(series);
+                    var newIndex = series.dummyData;
+    
+                    var dx = (newIndex - trueIndex + middle - newMiddle) * delta
+    
+                    series.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                    series.bulletsContainer.animate({ property: "dx", to: dx }, series.interpolationDuration, series.interpolationEasing);
+                })
+            }
+        }
+    }
+    
+    }); // end am4core.ready()
 
 
 }
